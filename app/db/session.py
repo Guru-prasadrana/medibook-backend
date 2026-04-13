546c23ff4225b913d22a75bb8f6c23cc8c0935b9
Guru-prasadrana <guruprasadrana571@gmail.com>
2026-04-13 11:56:18 +0530

    fix db connection

--- app/db/session.py
+++ app/db/session.py
@@ -1,7 +1,24 @@
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+
+# DATABASE_URL = "postgresql://postgres:12345@localhost:5432/medibook"
+
+# engine = create_engine(DATABASE_URL)
+# SessionLocal = sessionmaker(bind=engine)
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://postgres:12345@localhost:5432/medibook"
+# load .env for local
+load_dotenv()
 
-engine = create_engine(DATABASE_URL)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    connect_args={"sslmode": "require"} if "render.com" in DATABASE_URL else {}
+)
+
 SessionLocal = sessionmaker(bind=engine)
